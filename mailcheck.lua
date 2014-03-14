1794781e687531ecafd403d9962689ea4d9407e7
Kamil Essekkat <kamil@essekkat.pl>
2014-03-14 12:10:36 +0100

    Working version (only for maildirs)

--- mailcheck.lua
+++ mailcheck.lua
@@ -1,3 +1,5 @@
+#!/usr/bin/env lua
+
 -- Check for mail in maildirs.
 -- Rewrite in lua of http://mailcheck.sourceforge.net/
 version = '1.0'
@@ -12,6 +14,10 @@ globals.new = 0
 function read_config(configfile)
     local maildirs = {}
     local f = io.open(configfile)
+    if f == nil then
+        print( "Invalid config " .. configfile )
+        os.exit(1)
+    end
     while true do
         local line = f:read("*line")
         if line == nil then break end
@@ -40,7 +46,7 @@ end
 function count_files(path)
     local cnt = 0
     for file in lfs.dir(path) do
-        if file:find(":") ~= nil  then
+        if file:find("=") ~= nil  then
             cnt = cnt + 1
         end
     end
@@ -65,7 +71,11 @@ function pj(...)
     sep = '/'
     result = ''
     for i, v in ipairs(arg) do
-        result = result .. tostring(v) .. sep
+        if i == 1 then
+            result = tostring(v)
+        else
+            result = result .. sep .. tostring(v)
+        end
     end
     return result
 end
@@ -78,38 +88,54 @@ function help()
     print("    -f  - specify alternative rcfile location")
     print("    -h  - show this help screen")
     print("mailcheck-lua version " .. version .. "")
+    os.exit(0)
 end
 
 function argparse()
-    local configfile = os.getenv("HOME") .. '/.mailcheckrc'
-    local silent = false
+    local configfile = pj(os.getenv("HOME"), '.mailcheckrc')
+    local summary = false
     local _getnext = 0
     for k, v in pairs(arg) do
-        if v == '-h' then 
-            help()
-        elseif v == '-s' then 
-            silent = true
-        elseif v == '-f' then 
-            _getnext = 1
-        end
         if _getnext == 1 then
             configfile = v
             _getnext = 0
+        else
+            if v == '-h' then 
+                help()
+            elseif v == '-s' then 
+                summary = true
+            elseif v == '-f' then 
+                _getnext = 1
+            end
+        end
+    end
+    return configfile, summary
+end
+
+
+function main()
+    local configfile, summary = argparse()
+    local maildirs = {}
+    for k, pth in pairs(read_config(configfile)) do
+        local _unread = count_unread( pj(pth, 'cur') )
+        local _new = count_files( pj(pth, 'new') )
+
+        globals.unread = globals.unread + _unread
+        globals.new = globals.new + _new
+        table.insert(maildirs, {name=pth, unread=_unread, new=_new})
+    end
+    if summary then
+        if (globals.new + globals.unread) > 0 then
+            print(string.format("%d new %d unread", globals.new, globals.unread))
+        end
+    else
+        for k, v in pairs(maildirs) do 
+            if (v.new + v.unread) > 0 then
+                print(string.format("%s: %d new and %d unread message(s)", v.name, v.new, v.unread))
+            end
         end
     end
 end
 
-argparse()
 
-function main()
-    for k, pth in pairs(read_config()) do
-        local unread = count_unread( pj(pth, 'cur') )
-        local new = count_files( pj(pth, 'new') )
-        globals.unread = globals.unread + unread
-        globals.new = globals.new + new
-    end
-    print(string.format("%d new %d unread", globals.new, globals.unread))
-end
-
-
--- main()
+main()
